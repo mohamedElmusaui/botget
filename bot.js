4be1cb11892e55fe4f2861422213103380170ca4
mohamedElmusaui <41437450+mohamedElmusaui@users.noreply.github.com>
2018-09-09 21:44:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,3 @@
-const Discord = require("discord.js");
-const ytdl = require("ytdl-core");
-const { Client, Util } = require('discord.js');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8"); 
-const queue = new Map();
-const hastebin = require('hastebin-gen');
-const client = new Discord.Client();
-  
 var ServerID = "487639982845067264"; //اي دي السيرفر
 var ChannelID = "487699889535582228";// اي دي الروم
 
