7595633bfd5813dd058fb38b5c8d231155213b2e
mohamedElmusaui <41437450+mohamedElmusaui@users.noreply.github.com>
2018-09-09 21:42:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,14 +9,35 @@ const queue = new Map();
 const hastebin = require('hastebin-gen');
 const client = new Discord.Client();
   
-client.on('message', message => {
-if (message.content === "الحمد لله") {
-  var ms = 12000;
-      var interval = setInterval (function () {
-        message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-      }, ms); 
-    }
-});
+var ServerID = "487639982845067264"; //اي دي السيرفر
+var ChannelID = "487699889535582228";// اي دي الروم
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+// Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// Codes Server` - iiZeyad , Roýale"1.5k 🍷#1111
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
