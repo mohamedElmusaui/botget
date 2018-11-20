ded43f7048506e64bb7b959d1ca93fe3f4d8a2ad
mohamedElmusaui <41437450+mohamedElmusaui@users.noreply.github.com>
2018-11-20 12:06:12 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,12 +8,12 @@ client.on("message", message => {
             if (message.content.startsWith(prefix + "obc")) {
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
-  client.users.filter(m => m.presence.status !== 'online').forEach(m => {
- m.send(${argresult})\n ${m});
+  message.guild.members.filter(m => m.presence.status !== 'online').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
 })
- message.channel.send(\${client.users.filter(m => m.presence.status !== 'online').size}` : عدد الاعضاء المستلمين`); 
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
  message.delete(); 
-};
-});
+};     
+});;
 
 client.login(process.env.BOT_TOKEN);
